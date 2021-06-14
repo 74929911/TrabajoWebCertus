37e8521d8e9282513fa7f77e5852eea9537827f5
carloslorap <74977007@certus.edu.pe>
2021-06-13 22:42:14 -0500

    comint

---
+++ index.js
@@ -0,0 +1,76 @@
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='carlos-ghostunner/img1.jpg'
+    imagenes[1]='carlos-ghostunner/img2.jpg'
+    
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.carlos1.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 1) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    window.addEventListener('load' , function () { 
+        console.log('el contenido ha cargado');
+        
+        var imagenes =[]
+        
+        imagenes[0]='carlos-fornite/for1.jpg'
+        imagenes[1]='carlos-fornite/for2.jpg'
+        
+        
+        var indiceImagenes=0;
+        
+        function cambiarImagenes(){
+        
+            document.carlos2.src=imagenes[indiceImagenes];
+        
+        if (indiceImagenes < 1) {
+            indiceImagenes++; 
+            
+        }else{
+        indiceImagenes = 0;
+        }
+        
+        }
+        
+        setInterval(cambiarImagenes,3000);
+        
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
